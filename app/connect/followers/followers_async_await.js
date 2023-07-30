4da02a3249cd47f892b86e6e7e11459722d9a8d7
ballingj <ballingj@yahoo.com>
2023-07-30 10:31:00 -0500

    Lesson 11: nextjs server action

--- app/connect/followers/followers_async_await.js
+++ app/connect/followers/followers_async_await.js
@@ -2,24 +2,20 @@
 
 import { useState, useEffect } from 'react';
 
-
-
 export default function Followers() {
     const [goalString, setGoalString] = useState('?/?');
 
-    async function getFollowers() {
-        const res = await fetch('https://spotify-api-wrapper.appspot.com/artist/david-kando')
-        const json = await res.json();
-        console.log(`json`, json);
-        if (json && json.artists && json.artists.items && json.artists.items.length) {
-            return json.artists.items[0].followers.total;
-            
+    useEffect (() => {
+        async function fetchData() {
+            const res = await fetch('https://spotify-api-wrapper.appspot.com/artist/david-kando')
+            const json = await res.json();
+            console.log(`json`, json);
+            if (json && json.artists && json.artists.items && json.artists.items.length) {
+                const followers = json.artists.items[0].followers.total;
+                setGoalString(`${followers}/?`);
+            }
         }
-    }
-
-    useEffect(() => {
-        const followers = getFollowers();
-        setGoalString(`${followers}/?`);
+        fetchData()
     }, []);
 
     return (
