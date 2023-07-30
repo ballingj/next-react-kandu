c73969e4df860b81ed10a5284ec5075b4277b18c
ballingj <ballingj@yahoo.com>
2023-07-29 19:35:07 -0500

    Lesson 11: nextjs server action

--- app/connect/followers/index.js
+++ app/connect/followers/index.js
@@ -3,6 +3,9 @@
 import { useState, useEffect } from 'react';
 import getFollowers from './get-followers';
 
+// the next line of code is a trick to get a dynamic number: if 'the current' value is 16, adding
+// it to an empty string converts it into a string '17' then .length makes it a number 2
+// it will essentially simplify to  Math.pow(10, 2) which is 100
 const followerGoal = ({ current }) => Math.pow(10, (current + '').length);
 
 export default function Followers() {
