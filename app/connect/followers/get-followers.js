450f1f6c5b4fe528c99a513cfff7ddf1b062d3e5
ballingj <ballingj@yahoo.com>
2023-07-29 19:23:27 -0500

    Lesson 11: nextjs server action

---
+++ app/connect/followers/get-followers.js
@@ -0,0 +1,15 @@
+'use server'
+
+export default async function getFollowers() {
+    const response = await fetch(
+        'https://spotify-api-wrapper.appspot.com/artist/david-kando',
+        { next: { revalidate: 60 } }
+    )
+    const json = await response.json()
+
+    if (json && json.artists && json.artists.items && json.artists.items.length) {
+      return json.artists.items[0].followers.total;
+    }
+
+    throw new Error('Error fetching follower count');
+}
